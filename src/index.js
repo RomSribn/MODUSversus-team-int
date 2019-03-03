52d24459aade6ff46b7516ab8c56618796ae8a04
RomSribn <steeper.fest@gmail.com>
2019-03-03 22:02:24 +0200

    pre-responsive-design
    Full markup with a little bit responsive (bags)

---
+++ src/index.js
@@ -0,0 +1,69 @@
+import "./main.css";
+// import "./js/raphael-2.1.4.min.js";
+// import "./js/justgage.js";
+import { Gauge } from "./js/gauge.js";
+
+const opts1 = {
+    angle: 0, /// The span of the gauge arc
+    lineWidth: 0.3, // The line thickness
+    pointer: {
+        length: 0, // Relative to gauge radius
+        strokeWidth: 0 // The thickness
+    },
+    colorStart: '#6FADCF',   // Colors
+    colorStop: '#e2534b',    // just experiment with them
+    strokeColor: '#76c7c0'   // to see which ones work best for you
+};
+const opts2 = {
+    angle: 0, /// The span of the gauge arc
+    lineWidth: 0.3, // The line thickness
+    pointer: {
+        length: 0, // Relative to gauge radius
+        strokeWidth: 0 // The thickness
+    },
+    colorStart: '#6FADCF',   // Colors
+    colorStop: '#e2534b',    // just experiment with them
+    strokeColor: '#76c7c0'   // to see which ones work best for you
+};
+const opts3 = {
+    angle: 0, /// The span of the gauge arc
+    lineWidth: 0.3, // The line thickness
+    pointer: {
+        length: 0, // Relative to gauge radius
+        strokeWidth: 0 // The thickness
+    },
+    colorStart: '#6FADCF',   // Colors
+    colorStop: '#e2534b',    // just experiment with them
+    strokeColor: '#76c7c0'   // to see which ones work best for you
+};
+const opts4 = {
+    angle: 0, /// The span of the gauge arc
+    lineWidth: 0.3, // The line thickness
+    pointer: {
+        length: 0, // Relative to gauge radius
+        strokeWidth: 0 // The thickness
+    },
+    colorStart: '#6FADCF',   // Colors
+    colorStop: '#e2534b',    // just experiment with them
+    strokeColor: '#76c7c0'   // to see which ones work best for you
+};
+
+const target1 = document.getElementById('g1'); // your canvas element
+const target2 = document.getElementById('g2'); // your canvas element
+const target3 = document.getElementById('g3'); // your canvas element
+
+const gauge1 = new Gauge(target1).setOptions(opts1); // create sexy gauge!
+gauge1.maxValue = 100; // set max gauge value
+gauge1.setMinValue(0);  // set min value
+gauge1.set(50); // set actual value
+
+const gauge2 = new Gauge(target2).setOptions(opts2); // create sexy gauge!
+gauge2.maxValue = 100; // set max gauge value
+gauge2.setMinValue(0);  // set min value
+gauge2.set(70); // set actual value
+
+const gauge3 = new Gauge(target3).setOptions(opts3); // create sexy gauge!
+gauge3.maxValue = 100; // set max gauge value
+gauge3.setMinValue(0);  // set min value
+gauge3.set(80); // set actual value
+
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    mode: 'production',
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: "babel-loader"
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                use: [
+                    {
+                        loader: "url-loader",
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'images',
+                            limit: 10000,
+                        },
+                    },
+                    'img-loader',
+                ]
+            }
+        ],
+
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'styles.css',
+        }),
+        new HtmlWebpackPlugin({
+            inject: true,
+            hash: true,
+            template: './src/index.html',
+        })
+    ],
+    devServer:{
+        publicPath: '/',
+        historyApiFallback: true,
+        noInfo: false,
+        quiet: false,
+        stats: 'errors-only',
+        clientLogLevel: 'warning',
+        compress: true,
+        port: 5000,
+    }
+};
+
+
